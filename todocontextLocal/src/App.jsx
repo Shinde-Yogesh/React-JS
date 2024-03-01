0dd1305a90c5b124e06f2674f42aaa08e2388e21
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-03-01 14:08:25 +0530

    Done with local storage and Context API

--- todocontextLocal/src/App.jsx
+++ todocontextLocal/src/App.jsx
@@ -3,6 +3,9 @@ import { TodoProvider } from './contexts'
 import './App.css'
 import { useEffect } from 'react'
 
+import {TodoForm} from './components/'
+import {TodoItem} from './components/'
+
 function App() {
   const [todos, setTodos] = useState([])
 
@@ -20,7 +23,7 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
   }
 
   //for inserting the value 
@@ -49,9 +52,25 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
+            <TodoForm />
           </div>
+
+
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+
+
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className='w-full'
+              >
+              <TodoItem todo={todo} />
+
+
+
+              </div>
+            ))}
+
           </div>
         </div>
       </div>
--- todocontextLocal/src/components/TodoItem.jsx
+++ todocontextLocal/src/components/TodoItem.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
 
 function TodoItem({ todo }) {
-    
 
+    //editable state
+    const [isTodoEditable, setIsTodoEditable] = useState(false)
+
+    const [todoMsg, setTodoMsg] = useState(todo.todo)
+    const { updateTodo, deleteTodo, toggleComplete } = useTodo()
+
+
+    //functionality
+    const editTodo = () => {
+        updateTodo(todo.id, { ...todo, todo: todoMsg })
+        setIsTodoEditable(false)
+    }
+
+    const toggleCompleted = () => {
+        toggleComplete(todo.id)
+    }
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
-            }`}
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                }`}
         >
             <input
                 type="checkbox"
@@ -17,9 +32,8 @@ function TodoItem({ todo }) {
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
