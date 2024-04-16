f3d35259e415c5bc28131d8d86b231d164bdcf3b
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-16 12:13:27 +0530

    tailwind add

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,19 +1,40 @@
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { useEffect, useState } from 'react'
 import './App.css'
 import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import{Footer, Header} from './components'
+
 function App() {
-  
-  const [loading ,setLoading]  = useState(true)
+
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
 
-  useEffect(() => {})
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
 
-  return (
-    <>
-      <h1>A Blog app with appwrite</h1>
-    </>
-  )
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO :  {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+
 }
 
 export default App
