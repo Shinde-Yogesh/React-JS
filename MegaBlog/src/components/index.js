1f4124931da135e56c2c0d9d62897db24b6ead0a
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-23 13:01:45 +0530

    store doesnt have  valid reducer error solve

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,18 +1,18 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE"
-import Signup from "../pages/Signup";
-import Login from "./Login"
-import Post from "./Post"
-import PostForm from "./post-form/PostForm"
-import Postcard from "./PostCard"
-import AuthLayout from './AuthLayout';
-
-export
-{
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
     Header,
     Footer,
     Container,
@@ -21,8 +21,10 @@ export
     RTE,
     Signup,
     Login,
-    Post,
+    Button,
     PostForm,
-    Postcard,
-    AuthLayout
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
--- MegaBlog/src/store/store.js
+++ MegaBlog/src/store/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import {combineReducers } from 'redux'
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
 
 const store = configureStore({
     reducer: {
-
+        auth : authSlice,
+        //TODO: add more slices here for posts
     }
-})
+});
 
-export default store
+
+export default store;
