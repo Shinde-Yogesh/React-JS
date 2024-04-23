ecec813c7c2ad75b290600c976a64753e8b49fed
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-04-23 12:27:10 +0530

    Create the page with injecting compnent

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -4,6 +4,12 @@ import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE"
+import Signup from "../pages/Signup";
+import Login from "./Login"
+import Post from "./Post"
+import PostForm from "./post-form/PostForm"
+import Postcard from "./PostCard"
+
 export
 {
     Header,
@@ -11,5 +17,10 @@ export
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    Postcard
 }
