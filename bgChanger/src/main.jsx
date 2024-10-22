0691b6cff32d2f0eeabedd406d88bfe122bfeccd
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-22 21:50:01 +0530

    the material ui card functionality added

--- bgChanger/src/main.jsx
+++ bgChanger/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import BgChanger from './components/BgChanger'; // Updated path
 import BgChangerAuto from './components/BgChangerAuto';
+import MediaControlCard from './components/MediaControlCard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BgChangerAuto />
+    <MediaControlCard />
   </React.StrictMode>
 );
+
